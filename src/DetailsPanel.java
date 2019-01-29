a4cc3d28dd74bdd2ce51ba050482d0c31698cfd1
Nati8 <nati.hristova8@abv.bg>
2019-01-30 00:47:55 +0200

    added remove button and behaviour to it and calculating the bill

--- src/DetailsPanel.java
+++ src/DetailsPanel.java
@@ -10,9 +10,10 @@ public class DetailsPanel extends JPanel{
         setPreferredSize(size);
         setBorder(BorderFactory.createTitledBorder("Foodland"));
 
-        String[] productsString = {"Select product","Apple","Banana","Cucumber","Watermelon"};
-        JComboBox products = new JComboBox(productsString);
-        
+
+        final String[][] productsString = {{"Select product", "Apple", "Banana", "Cucumber", "Watermelon"}};
+        JComboBox products = new JComboBox(productsString[0]);
+
         DefaultListModel modelSelectedProducts = new DefaultListModel();
         JList selectedProducts = new JList(modelSelectedProducts);
 
@@ -20,31 +21,57 @@ public class DetailsPanel extends JPanel{
         selectedProducts.setLayoutOrientation(JList.VERTICAL);
         selectedProducts.setVisibleRowCount(-1);
 
-        modelSelectedProducts.addElement("Apple");
-
-        DefaultListModel modelBill = new DefaultListModel();
-        JList bill = new JList(modelBill);
-
-        bill.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-        bill.setLayoutOrientation(JList.VERTICAL);
-        bill.setVisibleRowCount(-1);
-
-        modelBill.addElement(5.5);
-        modelBill.addElement(6.5);
-        modelBill.addElement(4);
+        JLabel billLabel = new JLabel();
 
         JButton addProducts = new JButton("Add");
-
+        final double[] bill = {0};
         addProducts.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String product = (String)products.getSelectedItem();
+
                 switch (product){
                         case "Apple":modelSelectedProducts.addElement("Apple"); break;
                         case "Banana":modelSelectedProducts.addElement("Banana"); break;
                         case "Cucumber":modelSelectedProducts.addElement("Cucumber"); break;
                         case "Watermelon":modelSelectedProducts.addElement("Watermelon"); break;
                     }
+
+                switch (product){
+                    case "Apple": bill[0] = bill[0] + 2.5; break;
+                    case "Banana":
+                        bill[0] = bill[0] + 3.5; break;
+                    case "Cucumber":
+                        bill[0] = bill[0] + 2.5; break;
+                    case "Watermelon":
+                        bill[0] = bill[0] + 2.5; break;
+                }
+                billLabel.setText(String.valueOf(bill[0]));
+            }
+        });
+
+        JButton removeProducts = new JButton("Remove");
+
+        removeProducts.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedProduct = (String)selectedProducts.getSelectedValue();
+                switch (selectedProduct){
+                    case "Apple":modelSelectedProducts.removeElement("Apple"); break;
+                    case "Banana":modelSelectedProducts.removeElement("Banana"); break;
+                    case "Cucumber":modelSelectedProducts.removeElement("Cucumber"); break;
+                    case "Watermelon":modelSelectedProducts.removeElement("Watermelon"); break;
+                }
+                switch (selectedProduct){
+                    case "Apple": bill[0] = bill[0] - 2.5; break;
+                    case "Banana":
+                        bill[0] = bill[0] - 3.5; break;
+                    case "Cucumber":
+                        bill[0] = bill[0] - 2.5; break;
+                    case "Watermelon":
+                        bill[0] = bill[0] - 2.5; break;
+                }
+                billLabel.setText(String.valueOf(bill[0]));
             }
         });
 
@@ -64,12 +91,16 @@ public class DetailsPanel extends JPanel{
 
         gc.gridx = 2;
         gc.gridy = 0;
-        add(bill, gc);
+        add(billLabel, gc);
 
         gc.anchor = GridBagConstraints.CENTER;
         gc.gridx = 0;
         gc.gridy = 1;
         add(addProducts, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 1;
+        add(removeProducts, gc);
 
     }
 }
